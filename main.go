fef7a641ca981ed8ee385526bec092c8ca1355d2
Brian Sterle <sterle.brian@gmail.com>
2023-05-16 17:44:32 -0400

    return useful responses in each endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -15,32 +16,23 @@ import (
 
 var hdfsClient *hdfs.Client
 
-// lazy load the hdfs client
-func getHdfsClient() *hdfs.Client {
-	if hdfsClient == nil {
-		client, err := hdfs.New("localhost:9000")
-		if err != nil {
-			log.Fatalf("failed to create hdfs client: %s", err)
-		}
-		hdfsClient = client
-	}
-	return hdfsClient
+type UploadResponse struct {
+	Path    string `json:"path"`
+	Written int64  `json:"written"`
 }
 
-func main() {
-	// create an hdfs client
-	defer hdfsClient.Close()
+type CopyResponse struct {
+	From           string        `json:"from"`
+	To             string        `json:"to"`
+	Written        int64         `json:"written"`
+	FilesRequested int64         `json:"filesRequested"`
+	FilesCopied    int64         `json:"filesCopied"`
+	CopyFailures   []CopyFailure `json:"copyFailures"`
+}
 
-	// bind functions to routes
-	http.HandleFunc("/copy", copy)
-	http.HandleFunc("/upload", upload)
-	log.Println("Listening on :8080...")
-
-	// start the http server
-	e := http.ListenAndServe(":8080", nil)
-	if e != nil {
-		log.Fatalf("failed to start http server: %s", e)
-	}
+type CopyFailure struct {
+	Path   string `json:"path"`
+	Reason string `json:"reason"`
 }
 
 // Uploads the incoming byte[] to the hdfs path provided by
@@ -73,24 +65,28 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	// write data from request body into the file
-	_, err = io.Copy(file, r.Body)
+	written, err := io.Copy(file, r.Body)
+
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error copying request body into file %s %s", fileName, err), http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusOK)
+	resp := UploadResponse{path, written}
+	json, _ := json.Marshal(resp)
+	w.Write(json)
 }
 
 // Reads all files in a given directory provided by 'from'
 // and uploads them to the user provided path 'to'
 func copy(w http.ResponseWriter, r *http.Request) {
+	// get params
 	from := r.URL.Query().Get("from")
 	to := r.URL.Query().Get("to")
 	if from == "" || to == "" {
 		http.Error(w, "'from' and 'to' query params must be provided.'", http.StatusBadRequest)
 		return
 	}
+
 	client := getHdfsClient()
 	fileInfos, err := client.ReadDir(from)
 	if err != nil {
@@ -99,7 +95,12 @@ func copy(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Wait group to synchronize goroutines
-	var wg sync.WaitGroup
+	var (
+		writtenBytes int64
+		copyFailures = make([]CopyFailure, 0)
+		mutex        sync.Mutex
+		wg           sync.WaitGroup
+	)
 
 	for _, fileInfo := range fileInfos {
 		if fileInfo.IsDir() {
@@ -110,6 +111,14 @@ func copy(w http.ResponseWriter, r *http.Request) {
 		wg.Add(1)
 
 		go func(file string) {
+			var failure CopyFailure
+			defer func() {
+				if failure != (CopyFailure{}) {
+					mutex.Lock()
+					copyFailures = append(copyFailures, failure)
+					mutex.Unlock()
+				}
+			}()
 			defer wg.Done()
 
 			path := filepath.Join(from, file)
@@ -117,17 +126,23 @@ func copy(w http.ResponseWriter, r *http.Request) {
 			reader, err := client.Open(path)
 			if err != nil {
 				log.Printf("Failed to read file %s\n", file)
+				failure = CopyFailure{path, err.Error()}
 				return
 			}
 			defer reader.Close()
 
 			// Create a buffer to store the file contents
 			var buf bytes.Buffer
-			_, err = io.Copy(&buf, reader)
+			written, err := io.Copy(&buf, reader)
 			if err != nil {
 				log.Printf("Failed to read file '%s': %s\n", file, err)
+				failure = CopyFailure{path, err.Error()}
 				return
 			}
+
+			mutex.Lock()
+			writtenBytes += written
+			mutex.Unlock()
 
 			// Define the URL of Service B to send the file to
 			uploadUrl := "http://localhost:8080/upload?fileName=" + file + "&to=" + to
@@ -136,6 +151,7 @@ func copy(w http.ResponseWriter, r *http.Request) {
 			req, err := http.NewRequest(http.MethodPost, uploadUrl, &buf)
 			if err != nil {
 				log.Printf("Failed to create request for file '%s': %s", reader, err)
+				failure = CopyFailure{path, err.Error()}
 				return
 			}
 
@@ -146,13 +162,16 @@ func copy(w http.ResponseWriter, r *http.Request) {
 			resp, err := http.DefaultClient.Do(req)
 			if err != nil {
 				log.Printf("Failed to send file '%s' to Service B: %s", file, err)
+				failure = CopyFailure{path, err.Error()}
 				return
 			}
 			defer resp.Body.Close()
 
 			// Check the response from Service B
 			if resp.StatusCode != http.StatusOK {
-				log.Printf("Service B returned non-OK status for file '%s': %d", file, resp.StatusCode)
+				msg := fmt.Sprintf("Service returned non-OK status for file '%s': %d", file, resp.StatusCode)
+				log.Println(msg)
+				failure = CopyFailure{path, msg}
 				return
 			}
 
@@ -162,5 +181,48 @@ func copy(w http.ResponseWriter, r *http.Request) {
 	}
 
 	wg.Wait()
-	w.WriteHeader(http.StatusOK)
+
+	resp := CopyResponse{
+		From:           from,
+		To:             to,
+		Written:        writtenBytes,
+		FilesRequested: int64(len(fileInfos)),
+		FilesCopied:    int64(len(fileInfos) - len(copyFailures)),
+		CopyFailures:   copyFailures,
+	}
+	json, _ := json.Marshal(resp)
+
+	if len(copyFailures) > 0 {
+		http.Error(w, string(json), http.StatusInternalServerError)
+		return
+	}
+	w.Write(json)
+}
+
+// lazy load the hdfs client
+func getHdfsClient() *hdfs.Client {
+	// namenode := os.Getenv("HDFS_HOST")
+	if hdfsClient == nil {
+		client, err := hdfs.New("localhost:9000")
+		if err != nil {
+			log.Fatalf("failed to create hdfs client: %s", err)
+		}
+		hdfsClient = client
+	}
+	return hdfsClient
+}
+
+func main() {
+	defer hdfsClient.Close()
+
+	// bind functions to routes
+	http.HandleFunc("/copy", copy)
+	http.HandleFunc("/upload", upload)
+	log.Println("Listening on :8080...")
+
+	// start the http server
+	e := http.ListenAndServe(":8080", nil)
+	if e != nil {
+		log.Fatalf("failed to start http server: %s", e)
+	}
 }
