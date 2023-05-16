10f561f22e1bc98294208a0852a53eebc0f04cfd
Brian Sterle <sterle.brian@gmail.com>
2023-05-16 11:44:44 -0400

    /upload and /copy stubs

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # cluster-fastcopy
 copy data between hdfs clusters blazingly fast
 
+# Motivation
+- We want the ability to transfer files in hdfs over network boundaries
+- Sometimes in enterprises there are network firewalls and partitions that prevent directly copying between clusters with a tool like HDFS
+- The application will facilitate data copies between hdfs clusters in these sort of scenarios
+- the most common use case will be copying down production data into nonprod dev/test clusters to facilitate testing
+
+
+# Flow
+the basic flow will be:
+
+- recieve a request to copy data from cluster1 to cluster2
+- read data from hdfs cluster1 into memory 
+- batch write data into hdfs cluster2 by sending it to a microservice that resides in cluster2's network partition
+- make heavy use of goroutines to make this all as fast as possible
+
 
 # dependencies
 - https://github.com/colinmarc/hdfs
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/colinmarc/hdfs"
 )
 
 func main() {
 	http.HandleFunc("/copy", handleCopy)
+	http.HandleFunc("/upload", handleUpload)
 	log.Println("Listening on :8080...")
 
 	err := http.ListenAndServe(":8080", nil)
@@ -15,12 +20,37 @@ func main() {
 	}
 }
 
+// Uploads the incoming byte[] to the hdfs path provided by
+// query param 'to'
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	to := r.URL.Query().Get("to")
+
+	if to == "" {
+		http.Error(w, "'to' query param must be provided.", http.StatusBadRequest)
+		return
+	}
+	client, err := hdfs.New("localhost:9000")
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to start hdfs client: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	buf := make([]byte, 59)
+	r.Body.Read(buf)
+	fmt.Println(buf)
+	client.MkdirAll(to, os.FileMode(0755))
+	w.WriteHeader(http.StatusOK)
+}
+
+// Reads all files in a given directory provided by 'from'
+// and uploads them to the user provided path 'to'
 func handleCopy(w http.ResponseWriter, r *http.Request) {
 	from := r.URL.Query().Get("from")
 	to := r.URL.Query().Get("to")
 	if from == "" || to == "" {
-		http.Error(w, "'from' and 'to' query params must be supplied.'", http.StatusBadRequest)
+		http.Error(w, "'from' and 'to' query params must be provided.'", http.StatusBadRequest)
 		return
 	}
-
+	w.WriteHeader(http.StatusOK)
 }
